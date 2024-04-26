8c3b7ace39bc667c5460ce13c2858a493e634e1e
Islam Mušić <islam.music@scv.si>
2024-04-26 09:47:26 +0200

    prve komponente

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,12 @@
 import './App.css'
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
 
 function App() {
 
   return (
       <>
-          <header data-bs-theme="dark">
-              <div className="collapse text-bg-dark" id="navbarHeader">
-                  <div className="container">
-                      <div className="row">
-                          <div className="col-sm-8 col-md-7 py-4">
-                              <h4>About</h4>
-                              <p className="text-body-secondary">Add some information about the album below, the author,
-                                  or any other background context. Make it a few sentences long so folks can pick up
-                                  some informative tidbits. Then, link them off to some social networking sites or
-                                  contact information.</p>
-                          </div>
-                          <div className="col-sm-4 offset-md-1 py-4">
-                              <h4>Contact</h4>
-                              <ul className="list-unstyled">
-                                  <li><a href="#" className="text-white">Follow on Twitter</a></li>
-                                  <li><a href="#" className="text-white">Like on Facebook</a></li>
-                                  <li><a href="#" className="text-white">Email me</a></li>
-                              </ul>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div className="navbar navbar-dark bg-dark shadow-sm">
-                  <div className="container">
-                      <a href="#" className="navbar-brand d-flex align-items-center">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"
-                               stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                               aria-hidden="true" className="me-2" viewBox="0 0 24 24">
-                              <path
-                                  d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
-                              <circle cx="12" cy="13" r="4"/>
-                          </svg>
-                          <strong>Album</strong>
-                      </a>
-                      <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                              data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false"
-                              aria-label="Toggle navigation">
-                          <span className="navbar-toggler-icon"></span>
-                      </button>
-                  </div>
-              </div>
-          </header>
+          <Header />
 
           <main>
 
@@ -98,17 +59,7 @@ function App() {
 
           </main>
 
-          <footer className="text-body-secondary py-5">
-              <div className="container">
-                  <p className="float-end mb-1">
-                      <a href="#">Back to top</a>
-                  </p>
-                  <p className="mb-1">Album example is &copy; Bootstrap, but please download and customize it for
-                      yourself!</p>
-                  <p className="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a
-                      href="/docs/5.3/getting-started/introduction/">getting started guide</a>.</p>
-              </div>
-          </footer>
+            <Footer />
       </>
   )
 }
