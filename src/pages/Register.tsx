024bb49e26b30a184e9f6a43ff304b8b588232d4
Islam Mušić <islam.music@scv.si>
2024-05-10 08:35:48 +0200

    Registracija

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -5,16 +5,31 @@ const Register = () => {
         <>
             <main className="form-signin w-100 m-auto">
                 <form>
-                    <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                    <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
                     <div className="form-floating">
-                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                        <label htmlFor="floatingInput">Email address</label>
+                        <input type="text" className="form-control" id="firstNameInput"
+                               placeholder="Input your first name"/>
+                        <label htmlFor="firstNameInput">First name</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="text" className="form-control" id="lastNameInput"
+                               placeholder="Input your last name"/>
+                        <label htmlFor="lastNameInput">Last name</label>
+                    </div>
+                    <div className="form-floating">
+                        <input type="email" className="form-control" id="emailInput" placeholder="name@example.com"/>
+                        <label htmlFor="emailInput">Email address</label>
                     </div>
                     <div className="form-floating">
                         <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
                         <label htmlFor="floatingPassword">Password</label>
                     </div>
-                    <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                    <div className="form-floating">
+                        <input type="password" className="form-control" id="floatingPassword2" placeholder="Repeat password"/>
+                        <label htmlFor="floatingPassword2">Repet password</label>
+                    </div>
+                    <button className="btn btn-primary w-100 py-2" type="submit">Register</button>
                 </form>
             </main>
         </>
