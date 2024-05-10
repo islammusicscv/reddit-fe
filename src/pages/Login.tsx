0a0c7cb60089668f49696f2509f0be263ff3002f
Islam Mušić <islam.music@scv.si>
2024-05-10 10:18:56 +0200

    Registracija

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,15 +1,49 @@
+import {SyntheticEvent, useState} from "react";
+import axios from "axios";
+import {Navigate} from "react-router-dom";
+
 const Login = () => {
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+
+    const url = "http://localhost:3000/auth/login";
+    const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        const data = {
+            email,
+            password,
+        }
+
+        console.log(data);
+
+        const res = await axios.post(url, data);
+
+        if (res.status === 201) {
+            setRedirect(true);
+        }
+
+    }
+
+    if (redirect) {
+        return <Navigate to="/" />;
+    }
+
     return (
         <>
             <main className="form-signin w-100 m-auto">
-                <form>
+                <form onSubmit={submit}>
                     <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
                     <div className="form-floating">
-                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"
+                        onChange={(e)=>setEmail(e.target.value)}/>
                         <label htmlFor="floatingInput">Email address</label>
                     </div>
                     <div className="form-floating">
-                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password"
+                        onChange={(e)=>setPassword(e.target.value)}/>
                         <label htmlFor="floatingPassword">Password</label>
                     </div>
                     <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
